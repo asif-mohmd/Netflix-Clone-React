c5583cb17ab542b3a13a83c50a2da069eaed18be
Asif Muhammed <asifasifpsps@gmail.com>
2022-09-08 23:51:56 +0530

    RowPost completed with dummy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div className="App">
    <NavBar/>
    <Banner/>
+   <RowPost/>
     </div>
   );
 } 
