788acffd1d3bd1c4e5d71adc0a850fa1e99c4953
Asif Muhammed <asifasifpsps@gmail.com>
2022-09-08 23:33:16 +0530

    Header and Banner section completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+   <NavBar/>
+   <Banner/>
+    </div>
+  );
+} 
+
+export default App;
